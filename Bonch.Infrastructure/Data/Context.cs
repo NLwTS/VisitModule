9dbb88992f2dbe1f002e06d263bfdf951355fe0a
Dwayne Thunder <mohovikov.denis@gmail.com>
2024-10-31 12:38:34 +0300

    Настройка завешена

--- Bonch.Infrastructure/Data/Context.cs
+++ Bonch.Infrastructure/Data/Context.cs
@@ -1,7 +1,15 @@
-﻿namespace Bonch.Infrastructure.Data
-{
-    public class Context
-    {
+﻿using Microsoft.EntityFrameworkCore;
 
+namespace Bonch.Infrastructure.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options) 
+        { 
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+        }
     }
 }
--- VisitModule/Program.cs
+++ VisitModule/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<Bonch.Infrastructure.Data.Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Database")));
 
 var app = builder.Build();
 
