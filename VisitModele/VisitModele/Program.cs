ce576ad067fb1cf68fd806a04e3cb5a10fee433b
Lintuq <91467814+Lintuq@users.noreply.github.com>
2024-11-04 22:18:04 +0300

    Add files via upload
    Начало

---
+++ VisitModele/VisitModele/Program.cs
@@ -0,0 +1,50 @@
+﻿
+using VisitModele.MemberUniversity; //Для демонстрации базового класса
+
+namespace VisitModele
+{
+    internal class Program
+    {
+        /*static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }*/
+
+        public static void Main(string[] args)
+        {
+            // Демонстрация работы базового класса
+            /*
+            UniversityMember member1 = new UniversityMember(
+             "Иван", "Иванов", "Иванович",
+             "Факультет компьютерных наук", "Кафедра программирования",
+             "Программирование", "ivan.ivanov@example.com",
+             "+79000000000", "Студент", "Мужской",
+             "01.01.2000", "Москва", "Россия", "Центральный"
+         );
+
+            UniversityMember member2 = new UniversityMember(
+                "Мария", "Петрова", "Сергеевна",
+                "Факультет математики", "Кафедра прикладной математики",
+                "Прикладная математика", "maria.petrova@example.com",
+                "+79000000001", "Студент", "Женский",
+                "02.02.1999", "Санкт-Петербург", "Россия", "Северо-Западный"
+            );
+
+            UniversityMember member3 = new UniversityMember(
+                "Алексей", "Сидоров", "Александрович",
+                "Факультет физики", "Кафедра теоретической физики",
+                "Теоретическая физика", "aleksey.sidorov@example.com",
+                "+79000000002", "Преподаватель", "Мужской",
+                "03.03.1985", "Екатеринбург", "Россия", "Уральский"
+            );
+
+            // Выводим информацию о каждом участнике
+            member1.displayInfo();
+            Console.WriteLine(); // Печатаем пустую строку для разделения
+            member2.displayInfo();
+            Console.WriteLine();
+            member3.displayInfo();
+            */
+        }
+    }
+}
