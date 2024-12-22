d3cfede1c73be4b87a226c303cdf9cce650d320a
Dwayne Thunder <mohovikov.denis@gmail.com>
2024-12-22 10:32:41 +0300

    Blazor РАБОТАЕТ

--- VisitModule.API/Program.cs
+++ VisitModule.API/Program.cs
@@ -3,12 +3,17 @@ using VisitModule.API;
 using VisitModule.API.Pages;
 using VisitModule.Infrastructure.Data;
 using VisitModule.Infrastructure.Repository;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using VisitModule.API.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
 builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
 builder.Services.AddDbContext<VisitContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IVisitRepository, VisitRepository>();
@@ -16,11 +21,20 @@ builder.Services.AddScoped<IVisitRepository, VisitRepository>();
 
 var app = builder.Build();
 
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
 
-app.UseStatusCodePagesWithReExecute("/404");
-app.UseAntiforgery();
+app.UseHttpsRedirection();
 
-app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
+app.UseStaticFiles();
 
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
 
 app.Run();
